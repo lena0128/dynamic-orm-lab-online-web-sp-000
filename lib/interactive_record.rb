17c374a38c51a52c59467324f0e4b24d8bdf21f6
Lena Hou <lenahou2013@gmail.com>
2021-05-20 22:15:08 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,11 +20,11 @@ class InteractiveRecord
     column_names.compact
   end
 
-def initialize(options={})
-  options.each do |property, value|
-    self.send("#{property}=", value)
-  end
-end
+  def initialize(options={})
+      options.each do |property, value|
+        self.send("#{property}=", value)
+      end
+    end
 
 def table_name_for_insert
   self.class.table_name
