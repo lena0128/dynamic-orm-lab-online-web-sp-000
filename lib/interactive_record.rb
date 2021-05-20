b5b73c8c4c408316d7812295e7d583c53f0d3897
Lena Hou <lenahou2013@gmail.com>
2021-05-20 22:27:44 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,4 +52,9 @@ def self.find_by_name(name)
   DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", [name])
 end
 
+def self.find_by(hash)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys[0].to_s} = '#{hash.values[0].to_s}'"
+    DB[:conn].execute(sql)
+  end
+
 end
